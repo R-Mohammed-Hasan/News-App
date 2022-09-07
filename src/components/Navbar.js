1b120d7052c9e0db700fd9933a8c48e62613c15c
R-Mohammed-Hasan <mohammed.hasan@fwsa.freshworks.com>
2022-09-07 20:55:09 +0530

    Added active navabr

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,59 +12,60 @@ export default function Navbar(){
     let parsedData = await data.json();
     console.log(parsedData.articles);
   }
-    return (
-      <div>
-        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark p-3">
-            <div className="container-fluid">
-              <Link className="navbar-brand  ms-3 ms-lg-0" to="/">News Center</Link>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-3 ms-lg-0">
-                <li className="nav-item dropdown">
-                    <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Profile
-                    </Link>
-                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <li><Link className="dropdown-item" to="/profile">Your Profile</Link></li>
-                      <li><Link className="dropdown-item" to="/editProfile">Edit Profile</Link></li>
-                      <li><hr className="dropdown-divider" /></li>
-                      <li><Link className="dropdown-item" to="/signOut">Sign Out</Link></li>
-                    </ul>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" aria-current="page" to="/">Home</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/about">About</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/business">Business</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/entertainment">Entertainment</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/health">Health</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/science">Science</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/sports">Sports</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/technology">Technology</Link>
-                  </li>
-                </ul>
-                <form className="d-flex" onSubmit={sendRequest} >
-                  <input className="form-control me-2" type="search" id="input" placeholder="Search" aria-label="Search" />
-                  <button className="btn btn-outline-light" type="submit">Search</button>
-                </form>
-              </div>
+
+  return (
+    <div>
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark p-3">
+          <div className="container-fluid">
+            <Link className="navbar-brand  ms-3 ms-lg-0" to="/">News Center</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-3 ms-lg-0">
+              <li className="nav-item dropdown">
+                  <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Profile
+                  </Link>
+                  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><Link className="dropdown-item"  to="/profile">Your Profile</Link></li>
+                    <li><Link className="dropdown-item" to="/editProfile">Edit Profile</Link></li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li><Link className="dropdown-item" to="/signOut">Sign Out</Link></li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Link className={`nav-link ${window.location.pathname === "/" ? "active":""}`}  aria-current="page" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={`nav-link ${window.location.pathname.includes("about") ? "active":""}`} to="/about">About</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={`nav-link ${window.location.pathname.includes("business") ? "active":""}`} to="/business">Business</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={`nav-link ${window.location.pathname.includes("entertainment") ? "active":""}`} to="/entertainment">Entertainment</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={`nav-link ${window.location.pathname.includes("health") ? "active":""}`} to="/health">Health</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={`nav-link ${window.location.pathname.includes("science") ? "active":""}`} to="/science">Science</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={`nav-link ${window.location.pathname.includes("sports") ? "active":""}`} to="/sports">Sports</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={`nav-link ${window.location.pathname.includes("technology") ? "active":""}`} to="/technology">Technology</Link>
+                </li>
+              </ul>
+              <form className="d-flex" onSubmit={sendRequest} >
+                <input className="form-control me-2" type="search" id="input" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-light" type="submit">Search</button>
+              </form>
             </div>
-        </nav>
-      </div>
-    )
+          </div>
+      </nav>
+    </div>
+  )
 }
